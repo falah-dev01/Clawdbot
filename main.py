84a2059885bef182245466c09ddd4113c0f2ad33
falah muhammed <falah.uxui@gmail.com>
2026-05-02 00:15:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,35 @@
 import os
-import requests
-import re
+import asyncio
 from flask import Flask
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-API_KEY = os.getenv("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")
-BOT_TOKEN ="8752843871:AAG1jOpUlzjZSyBqOFGBTho82xHBZ9flnjk" 
+BOT_TOKEN ="8752843871:AAG1jOpUlzjZSyBqOFGBTho82xHBZ9flnjk"
 
 app_web = Flask(__name__)
 
 @app_web.route("/")
 def home():
-    return "Bot is running!"
+    return "Bot is running"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("⚡ Bot working")
+    await update.message.reply_text("⚡ Bot working!")
 
 async def run_bot():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    await app.run_polling()
+    await app.initialize()
+    await app.start()
+    await app.updater.start_polling()
+
+def start_bot():
+    asyncio.run(run_bot())
 
 if __name__ == "__main__":
     import threading
 
-    # run telegram bot in background
-    threading.Thread(target=lambda: __import__("asyncio").run(run_bot())).start()
+    # Start Telegram bot in background
+    threading.Thread(target=start_bot).start()
 
-    # run web server (Render needs this)
+    # Start web server (Render needs this)
     app_web.run(host="0.0.0.0", port=10000)
